6790d8820ef8ad16f984bb83ceeef1f023fedf63
HayateXhublua <tp777927@gmail.com>
2026-05-05 00:26:31 +0700

    Update HayateX.lua

--- HayateX.lua
+++ HayateX.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/1aa30184ae237f96ba8e2bd5889e7178b1c50b98dd6821eb57a908974151feaf.lua"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/E0kErQNL"))()
